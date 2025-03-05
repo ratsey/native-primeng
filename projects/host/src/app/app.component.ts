75b12d2cb74831f90e58ce14e0120a886107d4be
ratsey <jens@ratsey.com>
2025-03-05 17:09:32 -0500

    [setup] Added PrimeNG

--- projects/host/src/app/app.component.ts
+++ projects/host/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import { RouterOutlet} from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet,  ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
